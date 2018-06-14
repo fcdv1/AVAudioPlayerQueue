d5f5b7cff0380159deeae55537c93811edb3542b
zfh <245185601@qq.com>
2018-06-14 11:36:40 +0800

    add demo

--- AVAudioPlayerQueue/ViewController.m
+++ AVAudioPlayerQueue/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "AVAudioPlayerQueueManager.h"
+@import AVFoundation;
 
 @interface ViewController ()
 
@@ -16,14 +18,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)playButtonPressed:(id)sender {
+    NSArray *task1 = @[@"one.m4a",@"two.m4a",@"three.m4a",@"four.m4a",@"five.m4a",@"six.m4a"];
+    
+    NSMutableArray *task2 = [NSMutableArray new];
+    for (int i = 1; i <= 6; i++) {
+        NSString *path = [[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"%d.mp3",i] ofType:nil];
+        NSData *data = [NSData dataWithContentsOfFile:path];
+        if (data) {
+            [task2 addObject:data];
+        }
+    }
+    [[AVAudioPlayerQueueManager sharedInstance] enqueueBundleAudioFiles:task1 withTaskIdentifer:@"task1" beginPlayCallBack:^(NSString *taskID) {
+        NSLog(@"%@：Start",taskID);
+    } endPlayCallBack:^(NSString *taskID) {
+        NSLog(@"%@：Finish",taskID);
+    }];
+    [[AVAudioPlayerQueueManager sharedInstance] enqueueAudioDatas:task2 withTaskIdentifer:@"task2" beginPlayCallBack:^(NSString *taskID) {
+        NSLog(@"%@：Start",taskID);
+    } endPlayCallBack:^(NSString *taskID) {
+        NSLog(@"%@：Finish",taskID);
+    }];
 }
+- (IBAction)cancelAllButtonPressed:(id)sender {
+    [[AVAudioPlayerQueueManager sharedInstance] cancelAllTask];
+}
+
 
 
 @end
